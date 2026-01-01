0ee8a8bc0dabd36adb8a6f8f6650e92133090976
ddicrescenzo <160015935+ddicrescenzo@users.noreply.github.com>
2026-01-02 00:41:12 +0100

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,5 +1,24 @@
 # static.py
 
 HTML = """
-Whatever you want
+Cutting down on sugar will help you lose weight.
+We have been convinced of this because the people who own everything also own the news.
+I made a smoothie for you.
+Sadie made a list of things she needs to buy.
+Her oven had a habit of setting off the fire alarm for no reason.
+I don't want to live in a mansion.
+The snake in the grass made me jump.
+I'll be back.
+You would somehow manage to make yourself understood.
+There is a big supermarket in my neighborhood.
+I only eat fish.
+I don't like the polluted atmosphere of big cities.
+I brought home the trophy.
+Tom wants a house with a yard and a white picket fence.
+She was hyped up and ready to get to work.
+They want Alaskan salmon for lunch.
+Can I use your pencil?
+My false teeth are stuck to it.
+Why not me?
+Are you sure that's not a moose?
 """
